da7f48099326f71f635e44fb5283aa805a65d0e2
Jeff Connelly <jeff.connelly6@gmail.com>
2018-03-20 16:11:24 -0600

    Can render images/titles from articles

--- app/TopicScreen.js
+++ app/TopicScreen.js
@@ -6,6 +6,7 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Image
 } from 'react-native';
 import { connect } from 'react-redux'
 import {store} from '%store'
@@ -14,18 +15,27 @@ import {store} from '%store'
 
 export class TopicScreen extends React.Component {
   componentDidUpdate() {
-    // console.log('mounted!');
     console.log('mount info is:', this.props.articles);
-  }
+}
 
   render() {
+    const articles = this.props.articles.map((article, index) => (
+      <View key={index}>
+      <Text>{article.title}</Text>
+      <Image
+        style={{width: 50, height: 50}}
+        source={{uri: `${article.urlToImage}`}}
+      />
+      </View>
+  ));
+   
     console.log('render info is:', this.props.articles);
     return (
       <View style={styles.container}>
         <TouchableOpacity onPress={() => this.props.navigation.goBack()}> 
         <Text>Go Back</Text>
         </TouchableOpacity>
-        <Text></Text>
+        {articles}
       </View>
     );
   }
