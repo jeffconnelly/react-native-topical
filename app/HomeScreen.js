3df47c2432b42344cb498790de4d6ac712769935
Jeff Connelly <jeff.connelly6@gmail.com>
2018-04-02 17:28:20 -0600

    Need to render button text live

--- app/HomeScreen.js
+++ app/HomeScreen.js
@@ -84,7 +84,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Avenir-Light'
   },
   button: {
-    padding: 15,
+    padding: 18,
     margin: 10,
     marginTop: 20,
     backgroundColor: '#64c5a5',
@@ -94,7 +94,7 @@ const styles = StyleSheet.create({
     width: 150,
     textAlign: 'center',
     color: 'white',
-    fontSize: 18,
+    fontSize: 14,
     overflow: 'hidden',
   },
   buttonWrapper: {
